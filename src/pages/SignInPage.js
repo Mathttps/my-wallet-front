2eaadbcb13b2bcd392fef4d6c909755b36667998
Matheus Henrique <mathttps@gmail.com>
2023-04-25 16:40:25 -0300

    pequenos ajustes

--- src/pages/SignInPage.js
+++ src/pages/SignInPage.js
@@ -1,18 +1,59 @@
 import styled from "styled-components"
 import { Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import React, { useState, useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import axios from "axios"
 
 export default function SignInPage() {
+
+  const [formData, setFormData] = useState({ email: "", password: "" })
+  const navigate = useNavigate()
+
+  function handleInputChange(event) {
+    const { name, value } = event.target
+    setFormData({ ...formData, [name]: value })
+  }
+
+  function login(event) {
+    event.preventDefault()
+
+    const URL = "http://localhost:5000/login"
+
+    axios.post(URL, formData)
+      .then(response => {
+        setToken(response.data.token)
+        navigate("/home")
+      })
+      .catch(error => {
+        alert(`Algo deu errado! Tente novamente mais tarde!`)
+        console.log(error)
+      })
+  }
+
   return (
     <SingInContainer>
-      <form>
+      <form onSubmit={login}>
         <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <button>Entrar</button>
+        <input placeholder="E-mail"
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleInputChange}
+          required
+        />
+        <input placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+          name="password"
+          value={formData.password}
+          onChange={handleInputChange}
+          required
+        />
+        <button type="submit">Entrar</button>
       </form>
 
-      <Link>
+      <Link to="/cadastro">
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,20 +1,73 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import axios from "axios"
+import { useState } from "react"
+
 
 export default function SignUpPage() {
+  const navigate = useNavigate()
+  const [form, setForm] = useState({ name: "", email: "", password: "" })
+
+  function handleForm(e) {
+    const { name, value } = e.target
+    setForm((prevForm) => ({ ...prevForm, [name]: value }))
+  }
+
+  async function register(m) {
+    m.preventDefault()
+
+    const URL = "http://localhost:5000/cadastro"
+
+    try {
+      const res = await axios.post(URL, form)
+      navigate("/sign-in")
+    } catch (err) {
+      alert(`Algo deu errado! \n${err.response.data.message} \n`)
+      console.error(err)
+    }
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={register}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
-        <button>Cadastrar</button>
+        <input placeholder="Nome"
+          type="text"
+          name="name"
+          value={form.name}
+          onChange={handleForm}
+          required
+        />
+
+        <input placeholder="E-mail"
+          type="email"
+          name="email"
+          value={form.email}
+          onChange={handleForm}
+          required
+        />
+
+        <input placeholder="Senha"
+          type="password"
+          autocomplete="new-password"
+          name="password"
+          value={form.password}
+          onChange={handleForm}
+          required
+        />
+
+        <input placeholder="Confirme a senha"
+          type="password"
+          autocomplete="new-password"
+          name="confirm-password"
+          required
+        />
+
+        <button type="submit">Cadastrar</button>
       </form>
 
-      <Link>
+      <Link  to="/">
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
